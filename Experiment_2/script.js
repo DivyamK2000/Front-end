1c434bd1b829a71a862023373049f750d704a123
Divyam <divyamkumar0607@gmail.com>
2024-02-09 04:16:54 +0530

    modified:   Experiment_2/index.html 	new file:   Experiment_2/script.js 	modified:   Experiment_2/style.css

---
+++ Experiment_2/script.js
@@ -0,0 +1,38 @@
+let Div = document.getElementById("movement");
+
+function isTouchDevice(){
+    try {
+        document.createEvent("TouchEvent");
+        return true;
+    }
+    catch(e) {
+        return false;
+    }
+}
+
+const move = (e) => {
+    try {
+        var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+        var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+    }
+    catch(e) {}
+    Div.style.left = x-40 + "px";
+    Div.style.top = y-40 + "px";
+};
+
+document.addEventListener("mousemove", (e) => {
+    move(e);
+});
+
+document.addEventListener("touchmove", (e) => {
+    move(e);
+});
+
+const img = document.getElementById("img");
+const color = document.getElementById("dark");
+const click = document.getElementById("back");
+
+click.addEventListener("click", function() {
+    img.src = "lit-bulb.png";
+    color.style.backgroundColor = 'transparent';
+})
